f26a50b490df45fa1e553f6255a3966c69e0ed8e
Shivam Mishra <117067899+shIVam18004@users.noreply.github.com>
2025-04-26 11:32:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById("incrementBtn").addEventListener("click", incrementCounter);
+
+document.getElementById("decrementBtn").addEventListener("click", decrementCounter);
+
+document.getElementById("resetBtn").addEventListener("click", resetCounter);
+
+
+let counterDisplay = document.getElementById("counterDisplay");
+let counterValue = 0;
+
+
+function updateCounterDisplay(){
+    counterDisplay.textContent = counterValue;
+}
+
+function incrementCounter(){
+    counterValue++;
+    updateCounterDisplay();
+}
+
+function decrementCounter(){
+    if(counterValue>0){
+        counterValue--;               // only this values gives -ive numbers if remove the if condition only
+        updateCounterDisplay();   
+    }
+}
+
+function resetCounter(){
+    counterValue =0;
+    updateCounterDisplay();
+}
